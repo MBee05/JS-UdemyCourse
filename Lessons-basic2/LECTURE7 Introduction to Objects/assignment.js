3ee77d023f380dd52f2b30b6b7e4689e1b2ad2c6
MBee05 <68609044+MBee05@users.noreply.github.com>
2022-09-12 16:54:42 +0200

    Lessons-basic2-Assignments-7

---
+++ Lessons-basic2/LECTURE7 Introduction to Objects/assignment.js
@@ -0,0 +1,16 @@
+// // ***LECTURE: Introduction to Objects
+// // 1. Create an object called 'myCountry' for a country of your choice, containing
+// // properties 'country', 'capital', 'language', 'population' and
+// // 'neighbours' (an array like we used in previous assignments)
+
+
+
+const myCountry1 = {
+    country : 'India',
+    capital : 'New Delhi',
+    language : 'Tamil',
+    population : 67,
+    neighbours : ['Pakistan', 'Nepal', 'Bangladesh'],
+
+    }
+
---
+++ Lessons-basic2/LECTURE7 Introduction to Objects/script.js
@@ -0,0 +1,12 @@
+
+// ***** Introduction to Objects *
+
+const  jonas = {
+    firstName : 'Jonas',
+    lastName  : 'Schmedtmann',
+    age : 2037 - 1991,
+    job : 'teacher',
+    friends : ['Mickael', 'Peter', 'Steven']
+};
+
+console.log(jonas);
